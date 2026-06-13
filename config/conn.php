d4f3a6a3f79f96084043d860d8a31d02224093a9
Ali Hassan <alohussan1@gmail.com>
2026-06-13 15:32:22 +0500

    database connection

---
+++ config/conn.php
@@ -0,0 +1,15 @@
+<?php
+$SERVERNAME = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DATABASE = "mobiles-site";
+
+$conn = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+if(!$conn){
+    die("Connection Failed");
+} else{
+    echo "connection successful";
+}
+
+?>
