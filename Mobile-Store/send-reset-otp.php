33de4e0f0b167a91553304aeb9e129189bbaa6fc
Ali Hassan <alohussan1@gmail.com>
2026-07-03 22:51:47 +0500

    send opt to verify email

---
+++ Mobile-Store/send-reset-otp.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+$name = $_SESSION['reset_name'] ;
+$email = $_SESSION['reset_email'] ;
+$otp = $_SESSION['reset_otp'];
+$otp_generated_at = $_SESSION['reset_otp_generated_at'];
+
+require_once "../vendor/autoload.php";
+require_once "../includes/google-config.php";
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = true;
+$mail->AuthType = 'LOGIN';
+$mail->Username = smtp_email;
+$mail->Password = smtp_password;
+$mail->SMTPSecure = PHPMailer :: ENCRYPTION_SMTPS ;
+$mail->Port = 465 ;
+
+$mail->setFrom( smtp_email, "Mobile Zone");
+$mail->addAddress($email, $name);
+$mail->isHTML(true);
+$mail->Subject = "Your OTP Verification code";
+$mail->Body = "  <h2>Email Verification</h2>
+<p>Hello <b>$name</b>,</p>
+<p>Your OTP is : </p>
+<h1>$otp</h1>
+<p>This OTP is valid for 5 minutes.</p>";
+
+
+try{
+    $mail->send();
+    header('location: verify-reset-otp.php');
+    exit();
+}catch(Exception $e){
+    echo  "Failed to send email.";
+}
+
+?>
