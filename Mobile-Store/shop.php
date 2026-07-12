a9a6e5a07d1687d484fc1f15642aa52282c6354a
Ali Hassan <alohussan1@gmail.com>
2026-07-12 21:51:09 +0500

    add navbar in shop page

--- Mobile-Store/shop.php
+++ Mobile-Store/shop.php
@@ -16,8 +16,8 @@ $upload = "../uploads/";
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Shop - MobileZone</title>
-  <link rel="stylesheet" href="shop-style.css" />
   <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="shop-style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 </head>
