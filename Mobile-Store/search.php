cad85c74c5f499c28ae7f03dc69996a99c3b3712
Ali Hassan <alohussan1@gmail.com>
2026-07-18 20:11:30 +0500

    fetch ID from mobile table

--- Mobile-Store/search.php
+++ Mobile-Store/search.php
@@ -7,9 +7,9 @@ if(!isset($_GET['keyword'])){
 
 $keyword = $_GET['keyword'];
 
-$stmt = mysqli_prepare($conn, "SELECT Brand, Model FROM mobiles where Brand LIKE ? OR Model LIKE ?");
+$stmt = mysqli_prepare($conn, "SELECT ID, Brand, Model FROM mobiles where Brand LIKE ? OR Model LIKE ?");
 $search = "%$keyword%";
-mysqli_stmt_bind_param($stmt, "ss", $search, $search);
+mysqli_stmt_bind_param($stmt, "ss",  $search, $search);
 mysqli_stmt_execute($stmt);
 
 $result = mysqli_stmt_get_result($stmt);
